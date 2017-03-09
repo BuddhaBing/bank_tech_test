8b9b30a4ed8458945f634be08099ab1b20883298
Rob Brentnall <rob.brentnall@gmail.com>
2017-03-09 10:39:21 +0000

    deposit (without date) done and tested

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,5 +6,8 @@ describe Account do
     it 'starts with nil balance' do
       expect(subject.balance).to be(0)
     end
+    it 'adds to the balance when a deposit is made' do
+      expect{subject.deposit(100)}.to change{subject.balance}.by(100)
+    end
   end
 end
