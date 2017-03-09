db1f755c6979049efd6ffb83c2bfafd2781a4c25
Rob Brentnall <rob.brentnall@gmail.com>
2017-03-09 11:33:21 +0000

    account done

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,22 @@
+require 'date'
+
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :history
 
   def initialize
     @balance = 0
+    @history = []
   end
 
-  def deposit(amount)
+  def deposit(date=Date.today, amount)
     @balance += amount
+    @history << { date => amount }
   end
 
-  def withdraw(amount)
+  def withdraw(date=Date.today, amount)
     @balance -= amount
+    @history << { date => -amount }
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,14 +9,54 @@ describe Account do
     it 'adds to the balance when a deposit is made' do
       expect{subject.deposit(100)}.to change{subject.balance}.by(100)
     end
+    it 'adds to the balance when multiple deposits are made' do
+      5.times do
+        subject.deposit(100)
+      end
+      expect(subject.balance).to be(500)
+    end
     it 'deducts from the balance when a withdrawal is made' do
       expect{subject.withdraw(50)}.to change{subject.balance}.by(-50)
     end
+    it 'deducts from the balance when multiple withdrawals are made' do
+      subject.deposit(500)
+      5.times do
+        subject.withdraw(50)
+      end
+      expect(subject.balance).to be(250)
+    end
   end
   context '#history' do
-    it 'stores deposit amounts and the dates they were made on' do
-      subject.deposit(100)
-      expect(subject.history.keys).to contain(Date.today)
+    context 'deposits' do
+      it 'stores deposit amounts' do
+        subject.deposit(100)
+        expect(subject.history.first.has_value?(100)).to be true
+      end
+      it 'stores the dates a deposit was made on' do
+        subject.deposit(100)
+        expect(subject.history.first.has_key?(Date.today)).to be true
+      end
+      it 'stores multiple deposits' do
+        subject.deposit(100)
+        subject.deposit(500)
+        expect(subject.history[0].has_value?(100) && subject.history[1].has_value?(500)).to be true
+      end
+    end
+    context 'withdrawals' do
+      it 'stores withdrawal amounts' do
+        subject.withdraw(100)
+        expect(subject.history.first.has_value?(-100)).to be true
+      end
+      it 'stores the dates a withdrawal was made on' do
+        subject.withdraw(100)
+        expect(subject.history.first.has_key?(Date.today)).to be true
+      end
+      it 'stores multiple withdrawals' do
+        subject.deposit(1000)
+        subject.withdraw(100)
+        subject.withdraw(500)
+        expect(subject.history[1].has_value?(-100) && subject.history[2].has_value?(-500)).to be true
+      end
     end
   end
 end
